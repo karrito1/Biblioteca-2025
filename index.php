7f97ef63d4a2905146ac69810a6add93fba897fa
karrito1 <kamilovilla554@gmail.com>
2025-09-29 07:58:37 -0500

    se agrego nuevo index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>BIBLIOTECA SENAP 2025</title>
+
+    <meta name="description" content="Biblioteca SENAP ofrece acceso a libros, recursos digitales y actividades culturales para fomentar el aprendizaje y la lectura. ¡Visítanos hoy!" />
+    <meta name="keywords" content="biblioteca, sala de lectura, educación, acceso a libros, libros digitales, actividades culturales" />
+    <meta name="robots" content="index, follow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="es" />
+    <meta name="author" content="Cristian Villa y Jhoan Morales" />
+    <meta name="revisit-after" content="30 days" />
+
+    <!-- Bootstrap CSS minificado -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Tu archivo CSS -->
+    <link rel="stylesheet" href="estilos.css" />
+</head>
+
+<body>
+
+
+    <!-- Bootstrap JS Bundle minificado (Popper incluido) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.4.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/068a4d5189.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
